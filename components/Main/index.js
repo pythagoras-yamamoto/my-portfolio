828115e4d859695100de0488e514b99c1179a2dc
pythagoras-yamamoto <yamamo8533@gmail.com>
2022-07-09 09:04:30 +0900

    init

---
+++ components/Main/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <>
+      <h1>hoge</h1>
+    </>
+  );
+};
+
+export default Main;
--- components/SideNavbar/index.js
+++ components/SideNavbar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Disclosure } from "@headlessui/react";
 import {
@@ -11,61 +12,71 @@ import {
 } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 import { FaRegComments } from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
 import { BiMessageSquareDots } from "react-icons/bi";
+import { IoIosPaper } from "react-icons/io";
 
 function SideNavbar() {
   return (
-    <div>
+    <div className="flex">
       <Disclosure as="nav">
         <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
           <GiHamburgerMenu
-            className="block md:hidden h-6 w-6"
+            className="block md:hidden h-8 w-8"
             aria-hidden="true"
           />
         </Disclosure.Button>
-        <div className="p-6 w-1/2 h-screen bg-white z-20 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+        <div className="overflow-auto p-6 w-1/2 h-screen bg-white -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
           <div className="flex flex-col justify-start item-center">
-            <h1 className="text-base text-center cursor-pointer font-bold text-blue-900 pb-1 w-full">
-              Yudai Yamamoto
-            </h1>
+            <Link href="/">
+              <h1 className="text-base text-center cursor-pointer font-bold text-gray-800 pb-1 w-full">
+                Yudai Yamamoto
+              </h1>
+            </Link>
             <p className="text-base text-center border-b text-gray-500 border-gray-100 pb-4 w-full">
-              @JAIST
+              Webエンジニア / 学生
             </p>
             <div className=" my-4 border-b border-gray-100 pb-4">
-              <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                <MdOutlineSpaceDashboard className="text-2xl text-gray-600 group-hover:text-white " />
-                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                  ダッシュボード
-                </h3>
-              </div>
               <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
                 <CgProfile className="text-2xl text-gray-600 group-hover:text-white " />
-                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                  プロフィール
-                </h3>
+                <Link href="/profile">
+                  <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                    プロフィール
+                  </h3>
+                </Link>
               </div>
               <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                <FaRegComments className="text-2xl text-gray-600 group-hover:text-white " />
+                <MdOutlineIntegrationInstructions className="text-2xl text-gray-600 group-hover:text-white " />
+                <Link href="/portfolio">
+                  <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                    ポートフォリオ
+                  </h3>
+                </Link>
+              </div>
+              <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <IoIosPaper className="text-2xl text-gray-600 group-hover:text-white " />
+                <Link href="/writing">
+                  <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                    発信・記事
+                  </h3>
+                </Link>
+              </div>
+              <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <AiOutlineHeart className="text-2xl text-gray-600 group-hover:text-white " />
                 <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                  チャット機能
+                  お気に入り
                 </h3>
               </div>
               <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
                 <MdOutlineAnalytics className="text-2xl text-gray-600 group-hover:text-white " />
                 <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                  分析レポート
+                  My データ
                 </h3>
               </div>
               <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
                 <BiMessageSquareDots className="text-2xl text-gray-600 group-hover:text-white " />
                 <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                  コメント
-                </h3>
-              </div>
-              <div className="flex  mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                <MdOutlineIntegrationInstructions className="text-2xl text-gray-600 group-hover:text-white " />
-                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                  ポートフォリオ
+                  チャット
                 </h3>
               </div>
             </div>
@@ -84,18 +95,18 @@ function SideNavbar() {
                 </h3>
               </div>
             </div>
-            {/* logout */}
             <div className=" my-4">
               <div className="flex mb-2 justify-start items-center gap-4 pl-5 border border-gray-200  hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
                 <MdOutlineLogout className="text-2xl text-gray-600 group-hover:text-white " />
                 <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                  Logout
+                  ログイン
                 </h3>
               </div>
             </div>
           </div>
         </div>
       </Disclosure>
+      <div className="w-full">hoge</div>
     </div>
   );
 }
